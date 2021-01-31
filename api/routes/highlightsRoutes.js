a0e8233dc601ad20bf70d3d58e546243054593a6
joaoe <joaoe@kyros.com.br>
2021-01-31 15:16:18 -0300

    alteracao routes

--- api/routes/highlightsRoutes.js
+++ api/routes/highlightsRoutes.js
@@ -2,12 +2,12 @@ module.exports = function(app) {
     var highlights = require('../controllers/highlightsController.js');
     var router = require("express").Router();
 
-    router.get('/extensao_api',highlights.list_highlights);
-    router.post('/extensao_api',highlights.create_highlight);
+    app.get('/highlights',highlights.list_highlights);
+    app.post('/highlights',highlights.create_highlight);
 /*     router.get('/:highlightId',highlights.read_highlights);
     router.put('/:highlightId',highlights.update_highlight);
     router.delete('/:highlightId',highlights.delete_highlight); */
 
-    app.use('/extensao_api/highlights', router);
     const customDomainReroute = require('@turinggroup/serverless-express-custom-domain-middleware').customDomainReroute;
+    app.use(customDomainReroute);
 };
