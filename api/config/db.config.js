8d92e519d716a734c499972b49c4ddcdb5c2de46
joaoe <joaoe@kyros.com.br>
2021-01-31 14:04:35 -0300

    alteracao mongodb string

--- api/config/db.config.js
+++ api/config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: "mongodb://localhost/highlightsDB"
+    url: "mongodb://extensaoadmin:Lookit2020@kamino.mongodb.umbler.com:51308/extensaousers?authSource=extensaousers&readPreference=primary&appname=MongoDB%20Compass&ssl=false"
 }
