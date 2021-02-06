e2c69e6bab1a00b98a7725ab57dfbad477f9cc64
joaoe <joaoe@kyros.com.br>
2021-02-05 21:41:45 -0300

    Redirecionamento https

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ child_process.execSync('npm install');
 
 var express = require('express');
 var app = express();
+
+//Redirecionamento HTTPS
+app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele 
+    if ((req.headers["x-forwarded-proto"] || "").endsWith("http")) //Checa se o protocolo informado nos headers é HTTP 
+        res.redirect(`https://${req.headers.host}${req.url}`); //Redireciona pra HTTPS 
+    else //Se a requisição já é HTTPS 
+        next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado 
+});
+//----------------
 var port = process.env.PORT || 3000;
 var db = require("./api/models");
 var mongoose = db.mongoose;
