8464bb18cca70cc6b0c9185fb51f85f5f596a7da
joaoe <joaoe@kyros.com.br>
2021-03-29 19:38:58 -0300

    Não é necessário autenticação para pegar rating

--- api/routes/rateRoutes.js
+++ api/routes/rateRoutes.js
@@ -3,6 +3,6 @@ const validation = require("../utils/auth");
 var rateController = require('../controllers/rateController.js');
 
 module.exports = function(app) {
-    app.get('/rate', validation.auth, rateController.get_rate);
+    app.get('/rate', rateController.get_rate);
     app.post('/rate', validation.auth, rateController.create_rate);
 };
