6424296994d7890b5960c7963508cccc1297f79f
joaoe <joaoe@kyros.com.br>
2021-04-06 12:33:24 -0300

    get others highlights

--- api/routes/highlightsRoutes.js
+++ api/routes/highlightsRoutes.js
@@ -4,6 +4,7 @@ var highlightsController = require('../controllers/highlightsController.js');
 
 module.exports = function(app) {
     app.get('/highlights', validation.auth, highlightsController.list_highlights);
+    app.get('/highlights/others', highlightsController.list_others_highlights);
     app.post('/highlights', validation.auth, highlightsController.create_highlight);
     app.delete('/highlights/:highlightId', validation.auth, highlightsController.delete_highlight);
 };
