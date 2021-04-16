4916c86901682e7d2b058a9633c5d6919f8948fe
joaoe <joaoe@kyros.com.br>
2021-04-16 19:39:08 -0300

    n sei

--- config/db.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    url: "mongodb://extensaoadmin:Lookit2020@kamino.mongodb.umbler.com:51308/extensaousers?authSource=extensaousers&readPreference=primary&appname=MongoDB%20Compass&ssl=false"
-}
